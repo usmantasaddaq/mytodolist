4d655adab9fbcaec657e2667b564b755107c0d31
usmantasaddaq <usmantasaddaq501@gmail.com>
2022-08-24 22:33:49 +0500

    RoboFriend app is build now

---
+++ src/Components/Scroll.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const scroll = (props)=>{
+    console.log(props)
+    return (
+        <div style={{overflowY: 'scroll' , border:'5px solid black', height: '800px' }}> {props.children}</div>
+    )
+};
+export default scroll;
